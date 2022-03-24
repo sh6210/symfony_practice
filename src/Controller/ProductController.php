4a118978377a305e16e86cdb3ff7ad3ca3957c5f
Sohag Hossain <shcseilacs@gmail.com>
2022-03-24 09:50:02 +0600

    wIP

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    public function index(ValidatorInterface $validator, ManagerRegistry $doctrine): Response
     {
-        return $this->render('product/index.html.twig', [
+        $entityManager = $doctrine->getManager();
+        $product = new Product();
+        $product->setName(null);
+        $product->setPrice('23');
+        $product->setDescription('Notheing special');
+
+        $errors = $validator->validate($product);
+
+        if (count($errors)){
+            return new Response("Error: {$errors}", 400);
+        }
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return new JsonResponse("Product created {$product->getId()}");
+
+        /*return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
-        ]);
+        ]);*/
     }
 }
